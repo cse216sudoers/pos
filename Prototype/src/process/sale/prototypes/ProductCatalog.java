5145dd0413336324066a107214397f2aee248eef
Noah <ndr217@lehigh.edu>
2015-10-02 11:26:49 -0400

    Update ProductCatalog
    added code to PC, push things into ProductDescription to be filled in

--- Prototype/src/process/sale/prototypes/ProductCatalog.java
+++ Prototype/src/process/sale/prototypes/ProductCatalog.java
@@ -3,6 +3,8 @@ package process.sale.prototypes;
 
 public class ProductCatalog {
     private ProductCatalog Cat;
+    private ProductDescription[] items;
+    private int itemCount=0;
     private ProductCatalog(){
         
     }
@@ -11,5 +13,16 @@ public class ProductCatalog {
             Cat=new ProductCatalog();
         }
         return Cat;
+    }
+    public void addItem(ProductDescription desc, int code, String name){
+        boolean isIn=false;
+        for (int i=0;i<itemCount;i++){
+            if (code==items[i].getCode()){
+                isIn=true;
+            }
+        }
+        if (!isIn){
+            items[itemCount]=new ProductDescription(desc, code, name);//NEEDS TO BE UPDATED WHEN CONSTRUCTOR IS MADE
+        }
     }
 }
--- Prototype/src/process/sale/prototypes/ProductDescription.java
+++ Prototype/src/process/sale/prototypes/ProductDescription.java
@@ -8,4 +8,12 @@ public class ProductDescription {
         this.productCode=productCode;
         this.price=price;
     }
+
+    ProductDescription(ProductDescription desc, int code, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int getCode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
