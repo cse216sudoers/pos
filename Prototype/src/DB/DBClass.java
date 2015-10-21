401ae35b26692a83ef2ea20f93c8575e55df04a7
Joshua Ryan <shua2727@gmail.com>
2015-10-21 11:33:09 -0400

    Add logic for select statements

--- Prototype/src/DB/DBClass.java
+++ Prototype/src/DB/DBClass.java
@@ -8,6 +8,7 @@ package DB;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,7 +26,7 @@ public class DBClass {
                 e.printStackTrace();
             }
         return br;
-    } 
+    }
     
     public static void insert(String path, String row){
 
@@ -37,6 +38,37 @@ public class DBClass {
 
     public static void update(String path, int id){
 
+    }
+    
+    /**
+     *
+     * @param path Path to the file you want to open
+     * @param column Name of the column you want to find
+     * @return The position of the column in this table, -1 if not found
+     */
+    public static int getColumnNumber(String path, String column){
+        BufferedReader table = open(path);
+        int columnNumber = -1;
+        column = column.toLowerCase();
+        String line = null;
+        
+        try {
+            line= table.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(DBClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String[] columns = line.split("|");
+        
+        for (int i = 0; i < columns.length; i++){
+            if (columns[i].toLowerCase().equals(column)){
+                columnNumber = i;
+                break;
+            }
+        }
+        
+        return columnNumber;
+        
     }
     
     public static int nextId(String path){
@@ -55,5 +87,40 @@ public class DBClass {
 
         return id;
     }
+    
+    /**
+     *
+     * @param path : Name of file you want to search 
+     * @param column : Name of column you want to test against
+     * @param value : Value of the column you want to compare to 
+     * @return : An ArrayList full of the rows that match where values in column
+     * are equal to value. Returns null if no rows matched.
+     */
+    public static ArrayList<String> select (String path, String column, String value){
+        int columnNumber = getColumnNumber(path, column);
+        String line;
+        ArrayList<String> rows = new ArrayList<String>();
+        
+        if (columnNumber == -1)
+            return null;
+        
+        BufferedReader table = open(path);
+    
+        try {
+            while ((line = table.readLine()) != null) {
+                String[] columns = line.split("|");
+                String test = columns[columnNumber];
+                if (test.equals(value)){
+                    rows.add(line);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(DBClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+        return rows;
+    }
+    
 
 }
