29cbeb630de020fc006607e23b0966ab81f777df
Peter Schaedler <pwschaedler@gmail.com>
2015-11-02 23:30:51 -0500

    Power toggle on MainWindow

--- Prototype/src/process/sale/prototypes/MainWindow.java
+++ Prototype/src/process/sale/prototypes/MainWindow.java
@@ -20,6 +20,9 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import javafx.scene.Node;
+
 /**
  *
  * @author peterschaedler
@@ -29,12 +32,15 @@ public class MainWindow {
     private Scene scene;
     private GridPane grid;
     private TextArea console;
+    private boolean powerState = true;
+    private ArrayList<Node> itemsOnScreen;
     
     public MainWindow() {
         super();
         grid = new GridPane();
         scene = new Scene(grid);
         stage = new Stage();
+        itemsOnScreen = new ArrayList<>();
         initWindow();
     }
     
@@ -59,8 +65,17 @@ public class MainWindow {
         buttonBox.setPadding(new Insets(0, 15, 0, 0));
         
         // Create buttons
+        Button powerButton = new Button("POWER");
+        powerButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                togglePower();
+            }
+        });
+        
         Button saleLabel = new Button("Sale");
         saleLabel.setMaxWidth(Double.MAX_VALUE);
+        itemsOnScreen.add(saleLabel);
         saleLabel.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
@@ -70,6 +85,7 @@ public class MainWindow {
         
         Button rentalLabel = new Button("Rental");
         rentalLabel.setMaxWidth(Double.MAX_VALUE);
+        itemsOnScreen.add(rentalLabel);
         rentalLabel.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
@@ -79,6 +95,7 @@ public class MainWindow {
         
         Button returnLabel = new Button("Return");
         returnLabel.setMaxWidth(Double.MAX_VALUE);
+        itemsOnScreen.add(returnLabel);
         returnLabel.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
@@ -87,7 +104,7 @@ public class MainWindow {
         });
         
         // Add buttons to grid
-        buttonBox.getChildren().addAll(saleLabel, rentalLabel, returnLabel);
+        buttonBox.getChildren().addAll(powerButton, saleLabel, rentalLabel, returnLabel);
         grid.add(buttonBox, 0, 0);
         
         // Create text area for writing receipts and things
@@ -96,6 +113,7 @@ public class MainWindow {
         console.setPrefRowCount(40);
         console.setEditable(false);
         grid.add(console, 1, 0);
+        itemsOnScreen.add(console);
         
         // Show the window
         stage.show();
@@ -106,6 +124,23 @@ public class MainWindow {
     
     public void writeToConsole(String text) {
         console.appendText("\n\n" + text);
+    }
+    
+    public void togglePower() {
+        if (powerState) {
+            disableAll(true);
+            powerState = false;
+        }
+        else {
+            disableAll(false);
+            powerState = true;
+        }
+    }
+    
+    public void disableAll(boolean disable) {
+        for (Node node : itemsOnScreen) {
+            node.setDisable(disable);
+        }
     }
     
     public void displayProcessSale() {
